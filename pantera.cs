ab598e6074765e47d8f484f186d1b4f79996c9ce
Joshua Navarro <9thcenturyboy@gmail.com>
2026-04-21 11:33:28 -0300

    soya

--- pantera.cs
+++ pantera.cs
@@ -1,10 +1,17 @@
 using System;
+using System.Collections.Generic;
 
 // CLASE CON MALA ARQUITECTURA (Todo es público y desprotegido)
-public class Heroe 
+public abstract class Heroe 
 {
     private string nombre; // Campo público: Peligroso [cite: 16, 41]
     private int puntosVida; // Campo público: Peligroso [cite: 16, 41]
+
+    public Heroe(string nombre, int puntosVida)
+    {
+        this.nombre = nombre;
+        this.puntosVida = puntosVida;
+    }
 
     public string Nombre
         {
@@ -17,18 +24,63 @@ public class Heroe
             get { return puntosVida; }
             set { puntosVida = value >= 0 ? value : 0; }
         }
+
+    public abstract void Atacar();
+   
 }
 
+public class Guerrero: Heroe 
+{
+    private int fuerza;
+    
+    public int Fuerza
+        {
+            get { return fuerza; }
+            set 
+            {
+                if(value > 0)
+                {
+                    fuerza = value;
+                }
+                else {fuerza = 0;}
+            }
+        }
+     public override void Atacar()
+    {
+        Console.WriteLine("El guerrero ataca con su espada usando " + fuerza + " de poder");
+    }
+
+    public Guerrero(string nombre, int puntosVida, int fuerza): base(nombre, puntosVida)
+    {
+        Fuerza = fuerza;
+    }
+}
+
+public class Mago : Heroe
+{
+    public override void Atacar()
+    {
+        Console.WriteLine("El mago lanza un hechizo de ataque.");
+    }
+
+     public Mago(string nombre, int puntosVida): base(nombre, puntosVida)
+    {
+        
+    }
+}
 class Program 
 {
     static void Main() // Punto de entrada obligatorio 
     {
-        Heroe miHeroe = new Heroe(); // Instanciación [cite: 10]
-        
-        miHeroe.Nombre = "Arturo";
-        miHeroe.PuntosVida = -500; // ¡ERROR LÓGICO! La salud no debería ser negativa.
+       List<Heroe> party = new List<Heroe>();
 
-        Console.WriteLine("Héroe: "+miHeroe.Nombre+", Vida: "+miHeroe.PuntosVida);
+       party.Add(new Guerrero("Conan", 100, 20));
+       party.Add(new Mago("Gandalf", 80));  
+        Console.WriteLine("comienza la batalla!");
+        foreach(var heroe in party)
+        {
+            heroe.Atacar();
+        }
         Console.ReadKey();
     }
 }
